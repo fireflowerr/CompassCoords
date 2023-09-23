6fbd8ba7187c469e470ecd85f0a94ab71b75cbf9
fireflowerr <36929366+fireflowerr@users.noreply.github.com>
2023-09-22 19:16:01 -0700

    Update for compatibility with v1.20.30

--- scripts/index.js
+++ scripts/index.js
@@ -6,17 +6,17 @@ const RUN_INTERVAL_SECONDS = 1;
 system.runInterval(() => {
     world.getAllPlayers().forEach((player) => {
         /**
-         * @type {import('@minecraft/server').EntityEquipmentInventoryComponent}
+         * @type {import('@minecraft/server').EntityEquippableComponent}
          */
-        const inventory = player.getComponent('minecraft:equipment_inventory');
-        const equipment = inventory.getEquipment(EquipmentSlot.mainhand);
+        const inventory = player.getComponent('minecraft:equippable');
+        const equipment = inventory.getEquipment(EquipmentSlot.Mainhand);
 
         // if not holding anything return
         if (!equipment) {
             return;
         }
 
-        const isHoldingCompass = equipment.type.id === 'minecraft:compass';
+        const isHoldingCompass = equipment.typeId === 'minecraft:compass';
         if (isHoldingCompass) {
             const {x, y, z} = player.location;
             player.onScreenDisplay.setActionBar(`coords: x:${x|0}, y:${y|0}, z:${z|0}`);
